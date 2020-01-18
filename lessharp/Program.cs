e3c472c276ee6918cfd60d6a00e0dd6b3a36738c
FreeFires <simonenkoff.a@gmail.com>
2020-01-18 11:59:59 +0200

    Lesson 6

--- lessharp/Program.cs
+++ lessharp/Program.cs
@@ -9,6 +9,19 @@ namespace lessharp
     {
         static void Main(string[] args)
         {
+            print("Привет мир", 25);
+            string name = "Георгий";
+            print(name, 59);
+            print("Helo WORLD!!!!", 21);
+            print();
+
+
+
+            Console.ReadKey();
+        }
+        public static void print(string words = "Hello", short number = 20)
+        {
+            Console.WriteLine("Переменная: {0}, число: {1}", words, number);
         }
     }
 }
